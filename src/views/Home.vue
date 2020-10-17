c38978648f700eb3868e32fb7d86148ab58cc8a1
Nicholas Villapiano <nicholas.villapiano@ibm.com>
2020-10-17 15:05:35 -0400

    Added npm start Added basic Colors Added basic Typography Added basic Variables Created scss structure Updated sandbox pages

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="card ae-bg-orange">
+      <h1>Hello world!</h1>
+      <h4 class="ae-invert-orange">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
+    </div>
+    <div class="card ae-bg-blue">
+      <h1>Hello world!</h1>
+      <h4 class="ae-invert-blue">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
+    </div>
+    <div class="card ae-bg-pink">
+      <h1>Hello world!</h1>
+      <h4 class="ae-invert-pink">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
+    </div>
+    <div class="card ae-bg-green">
+      <h1>Hello world!</h1>
+      <h4 class="ae-invert-green">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
+    </div>
+    <div class="card ae-bg-grey">
+      <h1>Hello world!</h1>
+      <h4 class="ae-invert-grey">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
+    </div>
+    <div class="card ae-bg-tan">
+      <h1>Hello world!</h1>
+      <h4 class="ae-invert-tan">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
+  // components: {
+  //   HelloWorld
+  // }
 }
 </script>
+
+<style lang="scss">
+
+  .card {
+    padding: 2rem;
+  }
+
+</style>

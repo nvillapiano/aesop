1f520c4026e2b4cf3e57b6c8adec45b34a4f46b1
Nicholas Villapiano <nicholas.villapiano@ibm.com>
2020-10-17 16:28:20 -0400

    AddedInputs

--- src/components/boilerplate.vue
+++ src/components/boilerplate.vue
@@ -2,8 +2,16 @@
   <div class="boilerplate" :class="`ae-bg-${color}`">
     <h1>Hello world!</h1>
     <h4 :class="`ae-invert-${color}`">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet tenetur tempora similique. Officiis fugiat ea dolorum veritatis iure aliquam, reprehenderit magnam autem, excepturi dolor necessitatibus aperiam nesciunt quae corrupti doloribus?</p>
+    <p style="max-width: 500px;">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet tenetur tempora similique. Officiis fugiat ea dolorum veritatis iure aliquam, reprehenderit magnam autem, excepturi dolor necessitatibus aperiam nesciunt quae corrupti doloribus?</p>
     <button class="ae-button">Button</button> <button class="ae-button ae-secondary">Secondary</button> <button class="ae-button ae-tertiary">Tertiary</button>
+    <br>
+    <form>
+      <input type="search" placeholder="Input"><button class="ae-button ae-secondary">Button</button>
+    </form>
+    <form action="">
+      <textarea name="" id="" cols="30" rows="10" placeholder="Textarea">Message...</textarea>
+    </form>
+
   </div>
 </template>
 
@@ -19,5 +27,10 @@ export default {
 <style scoped lang="scss">
   .boilerplate {
     padding: 2rem;
+  }
+
+  form {
+    display: flex;
+    align-items: center;
   }
 </style>
